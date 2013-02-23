7aa8369cdf10b89f3460bdcf69771c31dc9b7b60
Adam Ratana <adam.ratana@gmail.com>
2013-02-23 11:23:01 -0500

    more classes,packages

---
+++ PghRecycles/gen/com/pghrecycles/pghrecycles/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.pghrecycles.pghrecycles;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ PghRecycles/src/com/pghrecycles/pghrecycles/data/providers/DBPickupInfoProvider.java
@@ -0,0 +1,15 @@
+package com.pghrecycles.pghrecycles.data.providers;
+
+import android.text.format.Time;
+
+import com.pghrecycles.pghrecycles.data.LocationInfo;
+import com.pghrecycles.pghrecycles.data.PickupInfo;
+
+public class DBPickupInfoProvider implements PickupInfoProvider {
+
+	@Override
+	public PickupInfo getPickupInfo(LocationInfo locationInfo, Time year) {
+		return null;
+	}
+
+}
