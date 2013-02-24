cfed3b029e1b101705c3c4cacdbd99dc36959437
Alex Cline <alex.cline@gmail.com>
2013-02-24 05:25:51 -0500

    Clean Dashboard without buttons working.

---
+++ PghRecycles/src/com/pghrecycles/pghrecycles/Dashboard.java
@@ -0,0 +1,14 @@
+package com.pghrecycles.pghrecycles;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class Dashboard extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dashboard);
+	}
+
+}
