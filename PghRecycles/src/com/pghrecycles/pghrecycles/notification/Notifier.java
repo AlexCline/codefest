9b4efc8343e4ce0ff0b89185c6b3cd464fdccebe
Alexander Conrad <alexander.p.conrad@gmail.com>
2013-02-24 06:37:42 -0500

    uncommented notifier for commit

--- PghRecycles/src/com/pghrecycles/pghrecycles/notification/Notifier.java
+++ PghRecycles/src/com/pghrecycles/pghrecycles/notification/Notifier.java
@@ -11,21 +11,21 @@ import com.pghrecycles.pghrecycles.R;
 
 public class Notifier {
 	public static void notifyRealtime(Context context, CharSequence ticker, CharSequence title, CharSequence contentText) {
-//		Intent intent = new Intent(context, PghRecycles.class);
-//		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); 
-//		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//    	PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-//    	
-//    	NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-//	    Notification noti = new Notification.Builder(context).setDefaults(Notification.DEFAULT_ALL)
-//        .setContentTitle(title)
-//        .setTicker(ticker)
-//        .setContentText(contentText)
-//        //.setPriority(Notification.PRIORITY_DEFAULT)
-//        .setSmallIcon(R.drawable.ic_launcher)
-//        .setContentIntent(pendingIntent)
-//        .build();
-//	    
-//	    notificationManager.notify(R.layout.activity_pgh_recycles, noti);
+		Intent intent = new Intent(context, PghRecycles.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); 
+		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    	PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+    	
+    	NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+	    Notification noti = new Notification.Builder(context).setDefaults(Notification.DEFAULT_ALL)
+        .setContentTitle(title)
+        .setTicker(ticker)
+        .setContentText(contentText)
+        //.setPriority(Notification.PRIORITY_DEFAULT)
+        .setSmallIcon(R.drawable.ic_launcher)
+        .setContentIntent(pendingIntent)
+        .build();
+	    
+	    notificationManager.notify(R.layout.activity_pgh_recycles, noti);
 	}
 }
